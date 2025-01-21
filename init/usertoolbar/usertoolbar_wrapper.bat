6c6f273502c3425bb6a1b8e28b2f914b1a3b1cbd
AmperSam <81581391+AmperSam-git@users.noreply.github.com>
2025-01-21 13:22:36 -0330

    Add backup action to toolbar and user script

--- init/usertoolbar/usertoolbar_wrapper.bat
+++ init/usertoolbar/usertoolbar_wrapper.bat
@@ -13,6 +13,27 @@ set "ROOT_DIR=!WORKING_DIR:\tools\lunar_magic=!"
 :: Other Defines
 set "tools_dir=!ROOT_DIR!tools\"
 set "project_dir=!ROOT_DIR!workspace\"
+set "backup_dir=!ROOT_DIR!backups\"
+
+:: Backup Defines
+set "levels_dir=!backup_dir!levels\"
+set "map16_dir=!backup_dir!map16\"
+set "palette_dir=!backup_dir!palettes\"
+set "rom_dir=!backup_dir!rom\"
+
+:: Time stuff
+setlocal
+for /f "delims=" %%a in ('wmic OS Get localdatetime ^| find "."') do set DateTime=%%a
+
+set Year=%DateTime:~0,4%
+set Month=%DateTime:~4,2%
+set Day=%DateTime:~6,2%
+set Hour=%DateTime:~8,2%
+set Minute=%DateTime:~10,2%
+
+set "time_stamp=%Year%%Month%%Day%_%Hour%-%Minute%"
+
+:: Tools Defines
 
 :: addmusick
 set "amk_path=!tools_dir!addmusick"
@@ -34,6 +55,9 @@ set "uber_exe=UberASMTool.exe"
 set "asar_path=!tools_dir!asar"
 set "asar_list=!asar_path!\list.txt"
 set "asar_exe=asar.exe"
+:: lunar magic
+set "lm_path=!tools_dir!lunar_magic"
+set "lm_exe=Lunar Magic.exe"
 
 
 :ParseArgs
@@ -57,11 +81,16 @@ if /i "%~1"=="--run-tool" (
     set path=%~2
     set exe=%~3
     set param=%~4
-    set "romfile=%project_dir%\%~5"
+    set "rom_file=%project_dir%\%~5"
     goto :RunTool
 )
+if /i "%~1"=="--backup" (
+    set "rom_file=%project_dir%\%~2.smc"
+    set "rom_name=%~2"
+    goto :Backup
+)
 if /i "%~1"=="--asar" (
-    set "romfile=%project_dir%\%~2"
+    set "rom_file=%project_dir%\%~2"
     goto :RunAsar
 )
 goto :ParseArgs
@@ -70,12 +99,39 @@ goto :ParseArgs
 start "" /b "%file%"
 goto :Exit
 
+:Backup
+:: Export MWL level files
+if not exist %levels_dir%\%time_stamp% (
+    mkdir %levels_dir%\%time_stamp%
+)
+start "Lunar Magic" /i /wait "%lm_exe%" -ExportMultLevels %rom_file% %levels_dir%\%time_stamp%\level
+start "Lunar Magic" /i /wait "%lm_exe%" -ExportMultLevels %rom_file% %levels_dir%\level
+:: Export Map16
+if not exist %map16_dir% (
+    mkdir %map16_dir%
+)
+start "Lunar Magic" /i /wait "%lm_exe%" -ExportAllMap16 %rom_file% %backup_dir%\latest_AllMap16.map16
+start "Lunar Magic" /i /wait "%lm_exe%" -ExportAllMap16 %rom_file% %map16_dir%\%time_stamp%_AllMap16.map16
+:: Export Palettes
+if not exist %palette_dir% (
+    mkdir %palette_dir%
+)
+start "Lunar Magic" /i /wait "%lm_exe%" -ExportSharedPalette %rom_file% %backup_dir%\latest_Shared.pal
+start "Lunar Magic" /i /wait "%lm_exe%" -ExportSharedPalette %rom_file% %palette_dir%\%time_stamp%_Shared.pal
+:: Create time-stamped backup of your ROM
+if not exist %rom_dir% (
+    mkdir %rom_dir%
+)
+copy %rom_file% %rom_dir%\%time_stamp%_!rom_name!.smc
+copy %rom_file% %backup_dir%\latest_!rom_name!.smc
+goto :Exit
+
 :OpenDir
 start "" /b "%path%"
 goto :Exit
 
 :RunTool
-start "%exe%" /b /d %path% /i /wait %exe% %param% %romfile%
+start "%exe%" /b /d %path% /i /wait %exe% %param% %rom_file%
 echo Reload your ROM in Lunar Magic to see changes and before making more.
 pause
 exit
@@ -84,7 +140,7 @@ exit
 echo Applying patches with Asar...
 pushd %asar_path%
 for /f "tokens=*" %%a in ('findstr /v "^;" "%asar_list%"') do (
-    %asar_exe% -v "%%a" %romfile%
+    %asar_exe% -v "%%a" %rom_file%
 )
 popd
 echo Done
